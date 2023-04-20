164ab7a70d10c333c29434989eccea9391196824
Your Name <you@example.com>
2023-04-20 11:35:41 -0400

    book() and gain() added

--- Player.java
+++ Player.java
@@ -3,4 +3,11 @@ import java.util.ArrayList;
 public class Player{
     ArrayList cards = new ArrayList<String>();
 
+    public static void book(){
+
+    }
+
+    public static void gain(){
+
+    }
 }
