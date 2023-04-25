739191e9339892b14a02c0cae1e46c3e9ded6a73
Your Name <you@example.com>
2023-04-25 11:32:00 -0400

    Added methods to Player and SpawnPoint.

--- Player.java
+++ Player.java
@@ -2,12 +2,31 @@ import java.util.ArrayList;
 
 public class Player{
     ArrayList cards = new ArrayList<String>();
+    ArrayList inventory = new ArrayList<String>();
 
-    public static void book(){
+    public void book(){
 
     }
 
-    public static void gain(){
+    public void gain(){
 
+    }
+
+    /*
+     * Allows Player to move between locations.
+     */
+    public String move(String direction){
+        //will take in String direction such as "north" "south" "east" "west" and transport Player to the corresponding location if that direction is feasable.
+        return direction;
+    }
+
+    public void grab(String item){
+        inventory.add(item);
+    }
+
+    public String drop(String item){
+        inventory.remove(item);
+        String dropped = ("You are no longer carrying " + item);
+        return dropped;
     }
 }
---
+++ SpawnPoint.java
@@ -0,0 +1,16 @@
+public class SpawnPoint{
+    
+    /*
+     * Eta welcomes the new Player and introduces them to the game.
+     */
+    public void Eta(){
+
+    }
+
+    /*
+     * A description of the surrounding area.
+     */
+    public void description(){
+
+    }
+}
