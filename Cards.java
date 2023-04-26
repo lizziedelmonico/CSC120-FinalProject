35e74e907aeb5d6c2c2c7f9792d2270b3f5ec86b
Your Name <you@example.com>
2023-04-26 19:33:58 -0400

    Added cards

---
+++ Cards.java
@@ -0,0 +1,35 @@
+public class Cards {
+    
+    public void riskyDice(){
+
+    }
+
+    public void luckBankbook(){
+
+    }
+
+    public void fledglingGambler(){
+
+    }
+
+    public void angelsBreath(){
+
+    }
+
+    public void plotBeach(){
+
+    }
+
+    public void mysteryPond(){
+
+    }
+
+    public void nightJade(){
+
+    }
+
+    public void treePlenty(){
+
+    }
+
+}
